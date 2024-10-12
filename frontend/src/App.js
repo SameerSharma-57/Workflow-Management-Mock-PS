c14409f2d6f4a5cd57a4bccc19fef6d01ce7294b
prasangeet <b23ch1033@iitj.ac.in>
2024-10-13 02:59:03 +0530

    changed a bit of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthPage from './components/AuthPage';
 import Dashboard from './components/dashboard.jsx';
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
